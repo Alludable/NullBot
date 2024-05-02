391cc4bfcab5f7e37516ea13f39e4ad5c354492e
Alludable <110146328+Alludable@users.noreply.github.com>
2024-05-02 13:27:41 -0400

    Added fully fledged music playing capabilities (subject to change). Also revised automatic role adding upon the joining of the server.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 
 import discord
+import yt_dlp
 import requests, json
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -21,6 +22,16 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 API_KEY = os.getenv('API_KEY')
+
+ytdl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+            }],
+        }
+        
+ytdl = yt_dlp.YoutubeDL(ytdl_opts)
+ffmpeg_options = {'options': '-vn'}
 
 def greeting():
     file = open('bot_greetings.txt', 'r')
@@ -81,19 +92,15 @@ async def on_member_join(member):
             color=discord.Color.green()
         )
         
-    on_ready()
+    role = discord.utils.get(member.guild.roles, name='Followers')
+    
+    await member.add_roles(role)
+    await welcome_channel.send(f"{member.mention}, you have been assinged the role of {role}.")    
+    await on_ready()
         
 @client.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-
-@client.command()
-async def add_role(user: discord.Member, role : discord.Role = 1233094863533379736):
-    '''Adds the default role for a new server member'''
-    
-    if role in user.roles:
-        return
-    else: await user.add_roles(role)
+async def test(ctx, args):
+    await ctx.send(args)
     
         
 @client.event
@@ -120,14 +127,14 @@ async def on_member_remove(member):
         
 @client.command()
 async def gif(ctx,*,q="One Piece"):
-    api_key = 'JOIbHCfh7HUOEelNPQO9jYCO9hM9RRFT'
+    api_key = API_KEY
     api_instance = giphy_client.DefaultApi()
     
     try:
             api_responce = api_instance.gifs_search_get(api_key, q, limit=100, rating='g')
             lst = list(api_responce.data)
             giff = random.choice(lst)
-            print(giff)
+  
             embed = discord.Embed(title = q)
             embed.set_image(url=f"https://media.giphy.com/media/{giff.id}/giphy.gif")
             
@@ -137,8 +144,6 @@ async def gif(ctx,*,q="One Piece"):
     except ApiException as e:
         print(f"Exception from the API as: {e}")
         
-    finally:
-        print("If you didn't get the gif and the ApiException error wasn't thrown then, look here.")
         
 
 @client.event
@@ -152,6 +157,86 @@ async def on_message(message):
     
     if any(word in msg for word in words_list):
         await message.channel.send(f"Good morning {message.author}!")
-     
-client.run(TOKEN)
+        
+    await client.process_commands(message)
+    
+@client.command()
+async def play(ctx, url : str=r'https://www.youtube.com/watch?v=dQw4w9WgXcQ'):
+    try:
+       if url == r'https://www.youtube.com/watch?v=dQw4w9WgXcQ':
+           await ctx.channel.send("You didn't specify a url so I've chosen one for you. Enjoy :)") 
+       else:
+            voice_client = await ctx.author.voice.channel.connect()
+            voice_clients = [voice_client.guild.id]
+            voice_channel = discord.utils.get(ctx.guild.voice_channels, name="music-channel")
 
+            await ctx.channel.send(f"```{client.user} has connected to music-channel.```")
+            print(f"{client.user} has connected to {str(voice_channel)}.")
+    
+    except Exception as e:
+        print(e)
+        
+    try:
+        if url != r'https://www.youtube.com/watch?v=dQw4w9WgXcQ':
+            url = ctx.message.content.split()[1]
+        else:
+            url = url
+        
+        loop =  asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+        
+        song = data['url']
+        player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+        
+        voice_clients[ctx.message.guild.id].play(player)
+    
+    except Exception as e:
+        print(e)
+        
+        
+    
+@client.command()
+async def leave(ctx):
+    voice_channel = discord.utils.get(ctx.guild.voice_channels, name="music-channel")
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if voice != None:
+        if voice.is_connected():
+            await voice.disconnect()
+            print(f"{client.user} has disconnected from {str(voice_channel)}")
+    else:
+        await ctx.send("```I'm not currently connected to a voice channel.```")
+        
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if not ctx.voice.channel:
+        await ctx.channel.send("```I'm not currently in a voice channel.")
+        return
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("```I'm not currently playing any audio.```")
+
+@client.command
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if not ctx.voice.channel:
+        await ctx.channel.send("```I'm not currently in a voice channel.```")
+        return
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("```The audio hasn't been paused.```")
+        
+@client.command
+async def stop(ctx):
+    vc = ctx.VoiceClient.source
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if not ctx.voice.channel:
+        await ctx.channel.send("```I'm not currently in a voice channel.```")
+        return
+    else:
+        voice.stop()
+    
+            
+client.run(TOKEN)
